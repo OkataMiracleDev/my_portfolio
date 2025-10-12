7984652c2b7cb01695979e63432d2c33764f336b
Okata Miracle <okatamiracle.dev@gmail.com>
2025-10-12 16:44:17 +0100

    fix(PhotoCollage): update image source and dimensions
    The image was changed from "1.jpg" to "3.jpg" with adjusted width and height dimensions to better fit the component layout.

--- components/Home/About/PhotoCollage.tsx
+++ components/Home/About/PhotoCollage.tsx
@@ -5,7 +5,7 @@ const PhotoCollage = () => {
   return (
     <div className='relative rotate-25 hover:rotate-18 hover:scale-115 left-15 top-15 md:left-28 md:top-22 transition-all duration-400 w-fit h-fit'>
       <div className='flex flex-col items-center w-[10rem] h-[14rem] md:w-[20rem] md:h-[28rem] p-3 bg-white shadow-md shadow-gray-400'>
-        <Image src={"/images/1.jpg"} width={4000} height={4000} alt="1"  className='w-full  h-full object-cover rounded-lg drop-shadow-md drop-shadow-gray-300 cursor-pointer hover:scale-103 block transition-all duration-700' />
+        <Image src={"/images/3.jpg"} width={3000} height={3000} alt="1"  className='w-full  h-full object-cover rounded-lg drop-shadow-md drop-shadow-gray-300 cursor-pointer hover:scale-103 block transition-all duration-700' />
         <p className='text-gray-700 text-base md:text-lg'>@mimi_codes</p>
       </div>
     </div>
