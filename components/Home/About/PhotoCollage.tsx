abc94f6d486db30ae5e0cdcf76a724f0b0a5f47f
Okata Miracle <okatamiracle.dev@gmail.com>
2025-10-12 17:23:58 +0100

    style(Home): adjust responsive positioning and sizing of components
    - Update positioning values for About and HomeProjects sections to improve layout on different screen sizes
    - Modify PhotoCollage dimensions and rotation for better responsiveness

--- components/Home/About/PhotoCollage.tsx
+++ components/Home/About/PhotoCollage.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const PhotoCollage = () => {
   return (
-    <div className='relative rotate-25 hover:rotate-18 hover:scale-115 left-15 top-15 md:left-28 md:top-22 transition-all duration-400 w-fit h-fit'>
-      <div className='flex flex-col items-center w-[10rem] h-[14rem] md:w-[20rem] md:h-[28rem] p-3 bg-white shadow-md shadow-gray-400'>
+    <div className='relative rotate-18 lg:rotate-25 hover:rotate-18 hover:scale-115 left-19 top-15 md:left-52 lg:left-30 md:top-7 lg:top-22 transition-all duration-400 w-fit h-fit'>
+      <div className='flex flex-col items-center w-[12rem] h-[16rem] md:w-[16rem] md:h-[23rem] lg:w-[20rem] lg:h-[28rem] p-3 bg-white shadow-md shadow-gray-400'>
         <Image src={"/images/3.jpg"} width={3000} height={3000} alt="1"  className='w-full  h-full object-cover rounded-lg drop-shadow-md drop-shadow-gray-300 cursor-pointer hover:scale-103 block transition-all duration-700' />
         <p className='text-gray-700 text-base md:text-lg'>@mimi_codes</p>
       </div>
