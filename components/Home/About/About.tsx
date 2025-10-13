72c89ceceee486fa027fa7fef658f5b1810eca2d
Okata Miracle <okatamiracle.dev@gmail.com>
2025-10-13 16:08:29 +0100

    refactor(tailwind): rename screen breakpoint 'x' to 'xs' and update responsive styles
    - Renamed breakpoint for consistency with Tailwind conventions
    - Updated responsive positioning in About component
    - Optimized image dimensions in ProjectsSlider for better performance

--- components/Home/About/About.tsx
+++ components/Home/About/About.tsx
@@ -4,7 +4,7 @@ import PhotoCollage from './PhotoCollage'
 
 const About = () => {
   return (
-    <div id='about' className='absolute md:ml-4 lg:ml-[10%] top-[208vh] md:top-[180vh] lg:top-[140vh] xl:top-[216vh] left-4 md:left-auto translate-y-[-50%] lg:w-[40%]'>
+    <div id='about' className='absolute md:ml-4 lg:ml-[10%] top-[208vh] xs:top-[1000vh] md:top-[180vh] lg:top-[140vh] xl:top-[216vh] left-4 md:left-auto translate-y-[-50%] lg:w-[40%]'>
         <div className='relative z-50'>
             <SectionHeading heading='About mimi' />
             <div className='mt-4 mr-1 md:mr-15 lg:mr-0 md:mt-6 space-y-4 md:space-y-6'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
+      'xs': '380px',
       sm: '640px',
       md: '768px',
       lg: '1024px',
       xl: '1280px',
-      'x': '380px',
       '2xl': '1680px', // 👈 customized to match your desired width
     },
   },
