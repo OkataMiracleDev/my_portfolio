5dbc2785a4dc08e70ce42d69ab4d5d697363d103
Okata Miracle <okatamiracle.dev@gmail.com>
2025-11-05 20:04:46 +0100

    feat(Stack): add Node.js icon to tech stack display
    Add FaNode icon to the stack component to include Node.js in the displayed technologies. Also improve code formatting consistency by standardizing quote usage and JSX structure.

--- components/Home/Stack/Stack.tsx
+++ components/Home/Stack/Stack.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { ImHtmlFive2 } from "react-icons/im";
 import { DiCss3 } from "react-icons/di";
 import { SiTailwindcss } from "react-icons/si";
@@ -7,6 +7,7 @@ import { IoLogoJavascript, IoLogoReact } from "react-icons/io5";
 import { SiTypescript, SiGooglegemini } from "react-icons/si";
 import { RiNextjsFill, RiOpenaiFill } from "react-icons/ri";
 import { FaGit, FaGithub } from "react-icons/fa";
+import { FaNode } from "react-icons/fa";
 
 const Stack = () => {
   // store index of the active icon
@@ -24,12 +25,16 @@ const Stack = () => {
     FaGithub,
     SiGooglegemini,
     RiOpenaiFill,
+    FaNode,
   ];
 
   return (
-    <div data-aos="fade-up" className='relative w-full mt-[7rem] md:mt-[9rem] lg:mt-[10rem] 2xl:mt-[9rem] flex flex-col justify-center items-center'>
+    <div
+      data-aos="fade-up"
+      className="relative w-full mt-[7rem] md:mt-[9rem] lg:mt-[10rem] 2xl:mt-[9rem] flex flex-col justify-center items-center"
+    >
       {/* background text */}
-      <p className='absolute z-0 font-[Bungee_Inline] text-center text-8xl md:text-9xl opacity-20 select-none'>
+      <p className="absolute z-0 font-[Bungee_Inline] text-center text-8xl md:text-9xl opacity-20 select-none">
         My <br /> stack
       </p>
 
@@ -40,12 +45,15 @@ const Stack = () => {
             key={i}
             onClick={() => setActiveIndex(i)} // set clicked icon
             className={`cursor-pointer rounded-lg px-6 py-3 shadow-md transition-colors duration-300 flex justify-center items-center
-              ${activeIndex === i 
-                ? "bg-blue-200 text-gray-600 shadow-gray-500" // active look
-                : "bg-white text-black shadow-gray-300 hover:bg-blue-200 hover:text-gray-600 hover:shadow-gray-500"
+              ${
+                activeIndex === i
+                  ? "bg-blue-200 text-gray-600 shadow-gray-500" // active look
+                  : "bg-white text-black shadow-gray-300 hover:bg-blue-200 hover:text-gray-600 hover:shadow-gray-500"
               }`}
           >
-            <Icon className={`text-2xl ${Icon === RiOpenaiFill ? "scale-90" : ""}`} />
+            <Icon
+              className={`text-2xl ${Icon === RiOpenaiFill ? "scale-90" : ""}`}
+            />
           </div>
         ))}
       </div>
