3a51ef993471da1dd0af65d1bf6250ee9528106a
Okata Miracle <okatamiracle.dev@gmail.com>
2025-11-30 15:42:16 +0100

    style(Home/Hero): adjust hero section layout and positioning
    Update height, padding, and positioning of elements in the hero section to improve visual balance and spacing. Adjust button and decorative dot positions for better alignment across screen sizes.

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -8,9 +8,9 @@ const Hero = () => {
   const [openModal, setOpenModal] = useState(false);
 
   return (
-    <div className="relative h-[30vh] py-[45vh] md:py-[43vh]" id="home">
+    <div className="relative h-[50vh] top-[10rem]" id="home">
       <div className="relative w-full h-full text-[#22025a] font-[poppins] font-bold text-3xl md:text-6xl gap-4 md:gap-7 flex flex-col justify-center items-center">
-        <div className="bg-green-300 border-2 border-green-400 w-5 h-5 md:w-8 md:h-8 rounded-full absolute top-[53%] left-[61%] md:top-[45%] md:left-[59%] xl:top-[42%] xl:left-[57.4%] "></div>
+        <div className="bg-green-300 border-2 border-green-400 w-5 h-5 md:w-8 md:h-8 rounded-full absolute top-[52%] left-[65%] md:top-[46%] md:left-[57%] xl:top-[40%] xl:left-[57.4%] "></div>
         <Image
           src="/images/Miracle_Okata.jpg"
           alt="character"
@@ -24,7 +24,7 @@ const Hero = () => {
 
       <button
         onClick={() => setOpenModal(true)}
-        className="hire-me-btn font-[poppins] font-medium text-white backdrop-blur-sm bg-[#5a3b5e4b] shadow-md shadow-gray-500 px-4 py-2 rounded-xl cursor-pointer transition-all duration-300 flex absolute top-[28%] md:top-[30%] md:left-[60%] left-[64%] lg:left-[60%] xl:top-[28%] xl:left-[59%] translate-x-[-50%]"
+        className="hire-me-btn font-[poppins] font-medium text-white backdrop-blur-sm bg-[#5a3b5e4b] shadow-md shadow-gray-500 px-4 py-2 rounded-xl cursor-pointer transition-all duration-300 flex absolute top-[20%] md:left-[60%] left-[70%] lg:left-[60%] xl:top-[18%] xl:left-[59%] translate-x-[-50%]"
       >
         Hire Me!
       </button>
