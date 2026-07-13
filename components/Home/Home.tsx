5ba1072a0d1c1e9cf4104fa96d8494df3e43f2de
OkatamiracleDev <okatamiracle.dev@gmail.com>
2026-07-13 04:56:43 -0500

    chore: remove aos, redundant with GSAP ScrollTrigger

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,5 +1,4 @@
 "use client"
-import React, { useEffect } from 'react'
 import Hero from './Hero/Hero'
 import Projects from './Projects/Projects'
 import About from './About/About'
@@ -10,23 +9,8 @@ import Testimonials from './Testimonials/Testimonials'
 import Contact from './Contact/Contact'
 import Footer from './Footer/Footer'
 import SplashScreen from '../SplashScreen'
-import AOS from 'aos';
-import 'aos/dist/aos.css'; 
 
 const Home = () => {
-  useEffect(()=>{
-    const initAOS = async()=>{
-      await import('aos');
-      AOS.init({
-        duration:600,
-        easing:'ease-out',
-        once:true,
-        anchorPlacement: "top-bottom",
-      });
-    }
-    initAOS();
-  }, [])
-  
   return (
     <>
       <SplashScreen />
